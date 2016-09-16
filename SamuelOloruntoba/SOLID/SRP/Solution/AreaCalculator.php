b8c7d7b3d64db4c1fed80e1b87c9c449cfd7f310
Renato Martins <renato@goldmap.com.br>
2016-09-16 13:45:06 -0300

    A little adjust

--- SamuelOloruntoba/SOLID/SRP/Solution/AreaCalculator.php
+++ SamuelOloruntoba/SOLID/SRP/Solution/AreaCalculator.php
@@ -15,18 +15,4 @@ class AreaCalculator
     {
         // logic to sum the areas
     }
-
-    // Violation:
-    // The problem with the output method is that the AreaCalculator handles
-    // the logic to output the data. Therefore, what if the user wanted to
-    // output the data as json or something else?
-    public function output()
-    {
-        return implode('', array(
-            '<h1>',
-                'Sum of the areas of provided shapes: ',
-                $this->sum(),
-            '</h1>',
-        ));
-    }
 }
